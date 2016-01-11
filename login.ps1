ef146835966428449103b1962a977ed60103c07f
MatthewMcCracken <mmccracken@apeagers.com.au>
2016-01-11 10:43:17 +1000

    Powershell Login Script Add
    Powershell Login Script Add

---
+++ login.ps1
@@ -0,0 +1,5 @@
+$credential = Get-Credential
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
